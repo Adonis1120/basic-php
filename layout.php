2a5c708cb9f4bc0beb5da0f9f3593ee5df0db871
Adonis Imperial <adonisimperial@yahoo.com>
2025-08-08 00:17:15 +0800

    sharedHTMEntryPointAcrossAllPages

---
+++ layout.php
@@ -0,0 +1,8 @@
+<?php include_once 'partials/head.php' ?>
+<?php include_once 'partials/header.php' ?>
+
+<main>
+    <?php echo $slot ?? ''; ?>
+</main>
+
+<?php include_once 'partials/foot.php' ?>
---
+++ pages/basic_calculator.php
@@ -0,0 +1,83 @@
+<?php
+    include_once __DIR__ . '/../Classes/Order.php';
+    include_once __DIR__ . '/../data/order.php';
+?>
+
+<form method="POST">
+    <div class="row">
+        <div class="col">
+            <div class="card p-4 gap-4">
+                <div>
+                    <label class="form-lable">Name</label>
+                    <input name="name" class="form-control">
+                </div>
+
+                <div class="row">
+                    <div class="col">
+                        <div class="row">
+                            <div class="col-6">
+                                <label class="form-lable">Meal</label>
+                                <input name="meal" class="form-control">
+                            </div>
+
+                            <div class="col">
+                                <label class="form-lable">Qty</label>
+                                <input type="number" name="meal_qty" class="form-control">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col">
+                        <div class="row">
+                            <div class="col-6">
+                                <label class="form-lable">Drinks</label>
+                                <input name="drink" class="form-control">
+                            </div>
+
+                            <div class="col">
+                                <label class="form-lable">Qty</label>
+                                <input type="number" name="drink_qty" class="form-control">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="d-flex justify-content-end">
+                    <button type="submit" class="btn btn-primary">Compute</button>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card">
+                <div class="card-body">
+                    <?php if ($_POST && $error_message == "") { ?>
+                        <p>
+                            Name: <?= $name ?>
+                        </p>
+
+                        <p>
+                            Meal: <?= $meal_name ?>
+                            <?= $meal_qty . ' x ' . $meal_price . ' = ' . $meal_total ?>
+                        </p>
+
+                        <p>
+                            Drink: <?= $drink_name ?>
+                            <?= $drink_qty . ' x ' . $drink_price . ' = ' . $drink_total ?>
+                        </p>
+
+                        <p>
+                            Grand Total: <?= $grand_total ?>
+                        </p>
+                    <?php } else { ?>
+                        <p>
+                            Please make your order properly!
+                        </p>
+                    <?php } ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</form>
