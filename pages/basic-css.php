4c1cdcb0a4e690d14e8cb0c8c461a580c6a5db3d
Adonis Imperial <adonisimperial@yahoo.com>
2025-08-10 21:12:28 +0800

    cssTable

---
+++ pages/basic-css.php
@@ -0,0 +1,99 @@
+<?php include_once '../partials/head.php' ?>
+<?php include_once '../partials/header.php' ?>
+
+<h1>this is a heading</h1>
+
+<p>this is a paragraph</p>
+
+<img src="image.png">
+
+<div class="glorium" id="glorium1">
+    This Glorium1
+</div>
+
+<div class="glorium" id="glorium2">
+    This Glorium2
+</div>
+
+<div class="glorium" id="glorium3">
+    This Glorium3
+</div>
+
+<div class="meing">
+    <div class="glorium">
+        This Glorium4
+        <div class="gloriumjra">
+            This Gloriumjra
+        </div>
+        <p>paragraph under glorium</p>
+    </div>
+</div>
+
+<button>Button</button>
+
+<a type="button" class="btn btn-primary" href="">this is a link</a>
+
+<div class="glorium">
+    next to meing
+</div>
+
+<table>
+    <thead>
+        <tr>
+            <th>
+                Name
+            </th>
+            <th>
+                Age
+            </th>
+            <th>
+                Sex
+            </th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>
+                Glorium1
+            </td>
+            <td>
+                <table>
+                    <tr>
+                        <td>
+                            18
+                        </td>
+                    <tr>
+                        <td>
+                            18
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            18
+                        </td>
+                    </tr>
+                </table>
+            </td>
+            <td>
+                <table>
+                    <tr>
+                        <td>
+                            Female
+                        </td>
+                    <tr>
+                        <td>
+                            Female
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Female
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </tbody>
+</table>
+
+<?php include_once '../partials/foot.php' ?>
--- partials/head.php
+++ partials/head.php
@@ -11,7 +11,7 @@
     <meta name="keywords" content="HTML, CSS, JavaScript">
 
     <!-- Link to external CSS file -->
-    <!-- <link rel="stylesheet" href="style.css"> -->
+    <link rel="stylesheet" href="../style.css">
 
     <!-- Link to Bootstrap CSS - CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
